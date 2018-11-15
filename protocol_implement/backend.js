8b77033dd6cdcf09052b23b43ee9c99f7a36f8e7
test <belov.kirill.alex@gmail.com>
2018-11-15 14:36:51 +0300

    Complete registration procedure

--- protocol_implement/backend.js
+++ protocol_implement/backend.js
@@ -4,12 +4,30 @@ var client  = mqtt.connect('mqtt://192.168.43.251',1883)
 client.on('connect', function () {
   client.subscribe('init_master', function (err) {
     if (!err) {
-      client.publish('init_master', 'Hello mqtt')
     }
   })
 })
 
 client.on('message', function (topic, message) {
   // message is Buffer
-  console.log(message.toString())
+  console.log(message.toString());
+  if(topic == 'init_master')
+  {
+      var registerReqMsgJSON = JSON.parse(message.toString());
+      var hwid = registerReqMsgJSON.data.hw_id;
+      var deviceTopicName = "dev_" + hwid;
+      client.subscribe("be_" + hwid)
+      var registerRespMsgJSON =
+        {
+          mid: "",
+          data: []
+        };
+      var registerRespDataJSON =
+        {
+          status: "OK"
+        };
+      registerRespMsgJSON.data = registerRespDataJSON;
+      var registerRespMsgString = JSON.stringify(registerRespMsgJSON);
+      client.publish(deviceTopicName, registerRespMsgString);
+  }
 })
--- protocol_implement/sender.js
+++ protocol_implement/sender.js
@@ -4,25 +4,39 @@ var client  = mqtt.connect('mqtt://192.168.43.251',1883) //10.42.0.10
 client.on('connect', function () {
   client.subscribe('init_master', function (err) {
     if (!err) {
+      var hwid = "testdev";
+      client.subscribe("dev_" + hwid);
       var registerMsgJSON =
       {
         mid: "",
         data: []
-      }
+      };
 
       var registerJSON =
       {
         version: "1.0",
-        hw_id: "testdev",
+        hw_id: hwid,
         sensors: [],
         actions: []
       };
 
       registerMsgJSON.data = registerJSON;
 
-      var registerString = JSON.stringify(registerMsgJSON);;
+      var registerString = JSON.stringify(registerMsgJSON);
       //var {}
       client.publish('init_master', registerString)
     }
   })
+})
+
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString());
+  if(topic == 'dev_testdev')
+  {
+    var registerRespMsgJSON = JSON.parse(message.toString());
+    var status = registerRespMsgJSON.data.status;
+    console.log(status);
+  }
 })
