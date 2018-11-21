adf99ddd54270b2d2c72944a4ed1e6e59d69d96c
test <belov.kirill.alex@gmail.com>
2018-11-21 10:23:32 +0300

    Add mid, hwid and name to register message

--- protocol_implement/sender.js
+++ protocol_implement/sender.js
@@ -1,21 +1,23 @@
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://192.168.43.251',1883) //10.42.0.10
+var mqtt = require('mqtt');
+var client  = mqtt.connect('mqtt://10.42.0.10',1883); //10.42.0.10 //192.168.1.63
+var HWID = '0000000088013898';
+var NAME = 'group6';
 
 client.on('connect', function () {
   client.subscribe('init_master', function (err) {
     if (!err) {
-      var hwid = "testdev";
-      client.subscribe("dev_" + hwid);
+      client.subscribe('dev_' + HWID);
       var registerMsgJSON =
       {
-        mid: "",
+        mid: 'REGISTER',
         data: []
       };
 
       var registerJSON =
       {
-        version: "1.0",
-        hw_id: hwid,
+        version: '1.0',
+        hw_id: HWID,
+        name: NAME,
         sensors: [],
         actions: []
       };
@@ -33,7 +35,7 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
   // message is Buffer
   console.log(message.toString());
-  if(topic == 'dev_testdev')
+  if(topic == 'dev_' + HWID)
   {
     var registerRespMsgJSON = JSON.parse(message.toString());
     var status = registerRespMsgJSON.data.status;
